ce14eadaa06548704decd793ff0216893b5eae92
Andrea <andrea.berrocal95@gmail.com>
2022-01-07 16:45:26 -0700

    added annotations for required in blog.model / changed layout of create.cshtml / change layout of index.cshtml

--- Models/Blog.cs
+++ Models/Blog.cs
@@ -1,10 +1,17 @@
-﻿namespace NonTraditionalsBlog.Models
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace NonTraditionalsBlog.Models
 {
     public class Blog
     {
         public int Id { get; set; }
+        [Required]
         public string? Title { get; set; }
+        [Required]
         public string? Content { get; set; }
+        [Required]
+        [DisplayName("Time Stamp")]
         public DateTime TimeStamp { get; set; }
     }
 }
